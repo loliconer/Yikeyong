f9afcd0565f92ec1cb8abf0dd595d900f399af92
heyz-scott <jianyong.zhou@heyz.com>
2016-04-16 16:19:34 +0800

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,50 @@
+var gulp = require( 'gulp' );
+var uglify = require( 'gulp-uglify' );
+var minifyCss = require( 'gulp-minify-css' );
+var changed = require( 'gulp-changed' );
+var clean = require( 'gulp-dest-clean' );
+var less = require( 'gulp-less' );
+var livereload = require( 'gulp-livereload' );
+var modified = require( 'gulp-modified' );
+
+gulp.task('build',function () {
+	gulp.src('src/public/js/*.js')
+        .pipe(changed('dist/public/js'))
+        .pipe(gulp.dest('dist/public/js'));
+    
+    gulp.src('src/public/css/*.css')
+        .pipe(changed('dist/public/css'))
+        .pipe(gulp.dest('dist/public/css'));
+    
+    gulp.src('public/img/**/*')
+        .pipe(clean('dist/public/img'))
+        .pipe(changed('dist/public/img'))
+        .pipe(gulp.dest('dist/public/img'));
+});
+
+gulp.task( 'liveless', function () {
+    gulp.src( 'src/public/less/*.less' )
+        .pipe( modified( 'less' ) )
+        .pipe( less() )
+        .pipe( minifyCss() )
+        .pipe( gulp.dest( 'src/public/css' ) )
+        .pipe( livereload() );
+} );
+
+gulp.task( 'livejs', function () {
+    gulp.src( 'src/public/js/*.js' )
+        .pipe( modified( 'js' ) )
+        .pipe( livereload() );
+} );
+
+gulp.task( 'watch', function () {
+    livereload.listen( {
+        start: true
+    } );
+    gulp.watch( 'public/less/**/*.less', [ 'liveless' ] );
+    gulp.watch( 'public/js/*.js', [ 'livejs' ] );
+} );
+
+gulp.task( 'default', function () {
+    gulp.start( 'watch' );
+} );
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+var webpack = require( 'webpack' );
+
+module.exports = {
+    context: __dirname,
+    devtool: false,
+    entry: {
+        index: './src/public/js/index.js'
+    },
+    output: {
+        path: __dirname + "/src/public/js",
+        filename: '[name].bundle.js'
+    },
+    resolve: {
+        moduleDirectories: [
+            'js',
+            'node_modules'
+        ],
+        /*自动扩展文件名后缀，require模块的时候可以省略后缀*/
+        extensions: [ '', '.json', '.js', '.less' ]
+    },
+    plugins: [
+        // new webpack.HotModuleReplacementPlugin(),
+        // new webpack.NoErrorsPlugin(),
+        // new webpack.IgnorePlugin( /^\.\/locale$/, /moment$/ ),
+        /*new webpack.DefinePlugin( {
+            "process.env": {
+                "NODE_ENV": JSON.stringify( 'development' )
+            }
+        } ),*/
+        // new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.optimize.UglifyJsPlugin()
+        // new webpack.optimize.UglifyJsPlugin( { mangle: false, sourcemap: false } ),
+        // new webpack.optimize.CommonsChunkPlugin( 'common.js' )
+    ]
+};
