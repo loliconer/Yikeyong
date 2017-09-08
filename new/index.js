a9e9fb5586aeeb1f3e666dad1d04d65c1dc0e8ab
loliconer <vuedev@163.com>
2017-09-08 21:55:09 +0800

    Update

--- new/index.js
+++ new/index.js
@@ -7,7 +7,7 @@ fs.readFile('new/html.html', 'utf8', (err, data) => {
   if (err) throw err
 
   data = data.replace('index.bundle.js', `${name}.bundle.js`)
-  data = data.replace('page-', `page-${name}`)
+  data = data.replace('style.css', `${name}.css`)
 
   fs.writeFile(`html/${app}.html`, data, err2 => {
     if (err2) throw err2
@@ -25,9 +25,7 @@ fs.readFile('new/js.js', (err, data) => {
 fs.readFile('new/less.less', 'utf8', (err, data) => {
   if (err) throw err
 
-  data = data.replace('page-', `page-${name}`)
-
-  fs.writeFile(`src/less/Pages/${name}.less`, data, err2 => {
+  fs.writeFile(`src/less/${name}.less`, data, err2 => {
     if (err2) throw err2
   })
 })
