22b4d30b2f5dda90632048585ad1fd06104b826d
loliconer <vuedev@163.com>
2019-11-09 21:50:52 +0800

    test

--- src/pages/index/app.js
+++ src/pages/index/app.js
@@ -4,3 +4,10 @@ import App from './App.vue'
 new Vue({
   render: h => h(App)
 }).$mount('app')
+
+Vue.config.errorHandler = function (error) {
+  alert(error)
+}
+window.onerror = function (msg) {
+  alert(msg)
+}
